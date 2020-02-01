974e919567fde66f676b5ca4e1f3efda6190b5f9
Ali Taylor-Cipolla <alicipolla@outlook.com>
2020-01-31 18:12:40 -0800

    Binary Search woooo

---
+++ LinearSearch/BinarySearch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinearSearch
+{
+   public class BinarySearch
+    {
+        public static object binarySearch(int[] arr, int key)
+        {
+            int min = 0;
+            int max = arr.Length - 1;
+
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (key == arr[mid])
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+            return "None";
+        }
+    }
+}
